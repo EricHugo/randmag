1930e15ad729347bd9b12eec13524bbd871153d0
eric.hugoson <hugoson@evolbio.mpg.de>
2019-01-09 11:26:33 +0100

    implemented custom completeness function

--- randMAG/randMAG.py
+++ randMAG/randMAG.py
@@ -22,24 +22,26 @@ from scipy import stats
 from fitdist import FitDist
 
 def _worker(seq, dist_param, min_length, comp=1.0, contam=0.0):
-    name = os.path.basename(seq)
+    basename = os.path.basename(seq)
+    name = '.'.join(basename.split('.'))
     print(name)
     # find full length of genome
     genome, genome_length = get_seq_length(seq)
     # split into contig given sizes in loop
-    contigs = {len(contig): contig for contig in split_contigs(genome, dist_param)}
+    contigs = {name + " # " + len(contig): contig for contig in 
+               split_contigs(genome, dist_param)}
     #print(contigs)
     # remove contigs based on desired completeness
     # return dict of genome with list of contig sizes produced
     ## to be used for contamination simulation
     #print(genome_length)
-    return contigs, name.split('.')[0]
+    return contigs, name
 
 def get_seq_length(seq):
     seq_fasta = [seqi.seq for seqi in SeqIO.parse(seq, "fasta")]
     if len(seq_fasta) > 1:
         print("Warning, more than one contig in genome", file=sys.stderr)
-        print(len(seq_fasta))
+        print(len(seq_fasta), file=sys.stderr)
     # only takes the length of first contig, should be chromosome
     seq_length = len(seq_fasta[0])
     #print(type(seq_fasta[0]))
@@ -59,8 +61,24 @@ def split_contigs(seq, params, min_length=300):
             seq[0] = ''
         yield contig
 
+def alter_completeness(contigs, completeness):
+    # get the dict keys of contig lengths
+    contig_lengths = [int(length) for length in contigs.keys()]
+    # sum for complete genome length
+    total_length = sum(contig_lengths)
+    # try to remove contig lengths down to completeness
+    removed_contigs = []
+    while sum(removed_contigs) < (1 - float(completeness)) * total_length:
+        removed_contigs.append(random.choice(contig_lengths))
+        #print(removed_contigs)
+    new_contigs = {length: contigs[length] for length in
+                   contig_lengths if not length in removed_contigs}
+    new_completeness = 1 - (sum(removed_contigs) - total_length)
+    return new_contigs, new_completeness
+
 def output_randcontigs(name, genome):
     """Writes shuffled contigs to file"""
+    # give randomised names to avoid writing same file
     randname = ''.join([random.choice(string.ascii_lowercase)
                         for _ in range(8)])
     with open(name + '_' + randname + ".fna", "w") as handle:
@@ -79,12 +97,12 @@ def main():
     parser.add_argument("genome_tab", help="Genomes in .fna in list format")
     parser.add_argument("distribution", help="Set of lengths to base the "\
                                              "distribution on.")
-    parser.add_argument("-c", "--completeness", default="1.0-1.0",
+    parser.add_argument("-c", "--completeness", default="1.0",
                         help="Range of completeness levels to be produced. "\
-                             "Default=1.0-1.0")
-    parser.add_argument("-r", "--contamination", default="0.0-0.0",
+                             "Default=1.0")
+    parser.add_argument("-r", "--contamination", default="0.0",
                         help="Range of contamination to be included in "\
-                             "produced MAGs. Default=0.0-0.0")
+                             "produced MAGs. Default=0.0")
     parser.add_argument("-n", "--num", default=False, type=int,
                         help="Number of randomised MAGs to produce. Produces "\
                              "one randomised per provided genome by default.")
@@ -99,8 +117,8 @@ def main():
                     if not re.match('#|\n', dist)]
     # here get distribution and parameters
     f = FitDist(distances)
-    distribution = f.find_best_dist()
-    f.histogram(distribution)
+    #distribution = f.find_best_dist()
+    #f.histogram(distribution)
     params = stats.gamma.fit(distances)
     #print(params)
     # feed into worker with random sampling
@@ -110,6 +128,8 @@ def main():
     while args.num > 0:
         for seq in input_seqs:
             contigs, name = _worker(seq, params, min(distances))
+            contigs, completeness = alter_completeness(contigs,
+                                                       args.completeness)
             output_randcontigs(name, contigs)
             args.num -= 1
             #print(args.num)
